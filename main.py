834ebc78ac679429bc2d091c3cd5f2d90a72796f
Pietro <124562706+pfrap@users.noreply.github.com>
2025-07-04 00:18:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,18 +36,20 @@ st.sidebar.header("🎛️ Filtri Distinta")
 selected_macro = st.sidebar.selectbox("Macro sistema", sorted(df_distinta["MACRO_SISTEMA"].unique()))
 filtered_macro = df_distinta[df_distinta["MACRO_SISTEMA"] == selected_macro]
 
-selected_cod_sistema = st.sidebar.selectbox("Codice sistema", sorted(filtered_macro["COD_SISTEMA"].unique()))
-filtered_cod = filtered_macro[filtered_macro["COD_SISTEMA"] == selected_cod_sistema]
+selected_cod_sistema = st.sidebar.selectbox("Codice sistema", sorted(filtered_macro["SISTEMA"].unique()))
+filtered_cod = filtered_macro[filtered_macro["SISTEMA"] == selected_cod_sistema]
 
-selected_c1 = st.sidebar.selectbox("C1", sorted(filtered_cod["C1"].unique()))
-filtered_c1 = filtered_cod[filtered_cod["C1"] == selected_c1]
+selected_c1 = st.sidebar.selectbox("C1_DESCRIZIONE", sorted(filtered_cod["C1_DESCRIZIONE"].unique()))
+filtered_c1 = filtered_cod[filtered_cod["C1_DESCRIZIONE"] == selected_c1]
 
-selected_c2 = st.sidebar.selectbox("C2", sorted(filtered_c1["C2"].unique()))
-filtered_c2 = filtered_c1[filtered_c1["C2"] == selected_c2]
+
+selected_c2 = st.sidebar.selectbox("C2_DESCRIZIONE", sorted(filtered_c1["C2_DESCRIZIONE"].unique()))
+filtered_c2 = filtered_c1[filtered_c1["C2_DESCRIZIONE"] == selected_c2]
+
 
 # Selezione gruppo
 selected_padre = st.sidebar.selectbox("Codice articolo", sorted(filtered_c2["CONCAT_3"].unique()))
-df_filtered_distinta = df_distinta[df_distinta["CONCAT_3"] == selected_padre]
+df_filtered_distinta = df_distinta[df_distinta["CONCAT_3"] == selected_padre] ############ Questo è il df della distinta filtrata #############
 
 # --- Filtro barre ---
 articoli_filtrati = df_filtered_distinta["ARTICOLO_FIGLIO"].unique()
@@ -60,61 +62,99 @@ df_barre_lunghezza = df_barre_filtrato[df_barre_filtrato["L_BARRA"] == selected_
 finiture_disponibili = sorted(df_barre_lunghezza["FINITURA"].dropna().unique())
 selected_finitura = st.sidebar.selectbox("Finitura", finiture_disponibili)
 
-# --- Merge tabelle ---
-df_merged = pd.merge(
+# --- Merge tabelle --- ############ Questo è il df della distinta filtrata unito alla tabella delle barre disponibili in base al codice articolo #############
+df_merged_filtered = pd.merge(
     df_filtered_distinta,
     df_barre,
     how="left",
     on="ARTICOLO_FIGLIO"
 )
 
-df_merged = df_merged[
-    (df_merged["L_BARRA"] == selected_length) &
-    (df_merged["FINITURA"] == selected_finitura)
+############ Lo stesso dataframe viene poi filtrato in base a lunghezza barra e finitura, AND condition #############
+df_merged_filtered = df_merged_filtered[
+    (df_merged_filtered["L_BARRA"] == selected_length) &
+    (df_merged_filtered["FINITURA"] == selected_finitura)
 ]
 
 # Calcolo peso grezzo con conversioni numeriche corrette
-if not df_merged.empty:
-    df_merged["COEFFICIENTE"] = pd.to_numeric(df_merged["COEFFICIENTE"], errors='coerce').fillna(0)
-    df_merged["GR/ML"] = pd.to_numeric(df_merged["GR/ML"], errors='coerce').fillna(0)
-    df_merged["L_BARRA"] = pd.to_numeric(df_merged["L_BARRA"], errors='coerce').fillna(0)
+if not df_merged_filtered.empty:
+    df_merged_filtered["COEFFICIENTE"] = pd.to_numeric(df_merged_filtered["COEFFICIENTE"], errors='coerce').fillna(0)
+    df_merged_filtered["GR/ML"] = pd.to_numeric(df_merged_filtered["GR/ML"], errors='coerce').fillna(0)
+    df_merged_filtered["L_BARRA"] = pd.to_numeric(df_merged_filtered["L_BARRA"], errors='coerce').fillna(0)
 
-    df_merged["PESO_GREZZO_KG"] = (df_merged["COEFFICIENTE"] * df_merged["GR/ML"] * df_merged["L_BARRA"]) / 1000000
-    df_merged["KG/ML"] = df_merged["GR/ML"] / 1000
+    df_merged_filtered["PESO_GREZZO_KG"] = (df_merged_filtered["COEFFICIENTE"] * df_merged_filtered["GR/ML"] * df_merged_filtered["L_BARRA"]) / 1000000
+    df_merged_filtered["KG/ML"] = df_merged_filtered["GR/ML"] / 1000
 
 # --- Layout ---
 st.title("Distinta base parete Unifor")
 descrizione_finitura = df_barre[df_barre["FINITURA"] == selected_finitura]["DESCRIZIONE FINITURA"].iloc[0]
 st.markdown(f"Visualizzazione per finitura `{selected_finitura}, {descrizione_finitura}` e lunghezza `{selected_length} mm`")
 
+#st.dataframe(df_merged_filtered)
 
+# Info articolo e metriche
+if not df_merged_filtered.empty:
+    col0a, col0b,col0c = st.columns([1,1,1])
+    with col0a:
+        st.subheader(df_merged_filtered["CONCAT_3"].iloc[0])
+        st.markdown(f"{df_merged_filtered["ID_COMPONENTE_ARTICOLO_PADRE_DESCRIZIONE"].iloc[0]}")
 
-if not df_merged.empty:
-    df_summary = df_merged.groupby("CONCAT_3").agg({
-        "COD_SISTEMA": "first",
-        "SISTEMA": "first",
-        "C1": "first",
-        "C2": "first",
-        "ID_COMPONENTE_ARTICOLO_PADRE_DESCRIZIONE_BREVE": "first",
-        "UNIT_ARTICOLO_PADRE": "first",
-        "PESO_GREZZO_KG": "sum",
-        "KG/ML": "sum"
-    }).reset_index()
-    st.dataframe(df_summary, use_container_width=True)
+        st.markdown(f"***Codice sistema:*** {df_merged_filtered["COD_SISTEMA"].iloc[0]} - {df_merged_filtered["SISTEMA"].iloc[0]}")
+        st.markdown(f"***Categoria:*** {df_merged_filtered["C1"].iloc[0]} {df_merged_filtered["C2"].iloc[0]} - {df_merged_filtered["C2_DESCRIZIONE"].iloc[0]}")
+        st.markdown(f"***Unit:*** {df_merged_filtered["UNIT_ARTICOLO_PADRE"].iloc[0]} ")
+    with col0b:
+        st.metric("Peso barre grezze per gruppo", f"{df_merged_filtered['PESO_GREZZO_KG'].iloc[0]:.2f} kg")
+    with col0b:
+        st.metric("Peso specifico gruppo", f"{df_merged_filtered['KG/ML'].iloc[0]:.2f} kg/ml")
+
+    # Simulazione preordine alluminio
+    st.subheader("Simulazione preordine alluminio")
+    col5, col6 = st.columns([3,1])
+    with col5:
+        Qta_ordine = st.number_input(
+            "Inserisci quantità di pezzi o metri lineari se si tratta di orizzontali.",
+            min_value=1,
+            value=1,
+            step=1
+            )
     
-    col1, col2 = st.columns([2, 1])
-    with col1:
-        col3, col4 = st.columns([1, 1])
-        #st.markdown(f"Peso barre grezze per gruppo: `{df_summary['PESO_GREZZO_KG'].iloc[0]:.2f}kg`")
-        #st.markdown(f"Peso specifico gruppo: `{df_summary['KG/ML'].iloc[0]:.2f} kg/ml`")
-        with col3:
-            st.metric("Peso barre grezze per gruppo", f"{df_summary['PESO_GREZZO_KG'].iloc[0]:.2f} kg")
-        with col4:
-            st.metric("Peso specifico gruppo", f"{df_summary['KG/ML'].iloc[0]:.2f} kg/ml")
+        # Ora moltiplichi il peso o le quantità dei materiali figli per questa quantità
+        def formula_ml(Qta_ordine, L_barra):
+            Val=(Qta_ordine/(L_barra/1000))*1.1
+            return Val
+        def formula_cad(Qta_ordine):
+            Val=(Qta_ordine)
+            return Val
+        df_simulazione=df_merged_filtered
+        df_simulazione["PESO_TOTALE_KG"] = df_simulazione["PESO_GREZZO_KG"] * Qta_ordine
+        df_simulazione = df_simulazione.rename(columns={
+            "ARTICOLO_FIGLIO": "ARTICOLO",
+            "COEFFICIENTE": "COEF",
+            "CODICE": "SEMILAV",
+            "PESO_GREZZO_KG": "KG_BARRA",
+            "PESO_TOTALE_KG": "KG_TOT",
+            "QTA_BARRE": "QTA_BARRE"
+            })
+        if df_simulazione["UNIT_ARTICOLO_PADRE"].iloc[0]=="ML":
+            df_simulazione["QTA_BARRE"] = formula_ml(Qta_ordine,df_simulazione["L_BARRA"])
+            st.markdown(f"Formula q.tà barre:`(Qta_ordine/(L_barra/1000))*1.1`")
+        elif df_simulazione["UNIT_ARTICOLO_PADRE"].iloc[0]=="N.":
+            st.markdown(f"Formula q.tà barre:`Qta_barre = Qta_ordine`")
+            df_simulazione["QTA_BARRE"] = formula_cad(Qta_ordine)
 
+        st.dataframe(df_simulazione[[
+            "ARTICOLO",
+            "COEF",
+            "GR/ML",
+            "L_BARRA" ,
+            "SEMILAV",
+            "KG_BARRA",
+            "KG_TOT",
+            "QTA_BARRE"
+        ]])
+            
 else:
-    with col1: # type: ignore
-        st.warning("Nessun dato disponibile con i filtri selezionati.")
+    st.warning("Nessun dato disponibile con i filtri selezionati.")
 
 # --- Immagine gruppo ---
 img_row = df_filtered_distinta[df_filtered_distinta["CONCAT_3"] == selected_padre]
@@ -129,29 +169,29 @@ if not img_row.empty:
         width_percent = fixed_height / float(img.size[1])
         new_width = int((float(img.size[0]) * float(width_percent)))
         img_resized = img.resize((new_width, fixed_height), Image.LANCZOS)
-        with col2:
+        with col0c:
             st.image(img_resized, use_container_width=False)
     else:
-        with col2:
+        with col0c:
             st.warning("🖼️ Immagine non trovata nella cartella `images`")
 
 st.markdown("---")
 
 # --- Tabella componenti dettagliati ---
-if not df_merged.empty:
-    df_merged = df_merged.rename(columns={'ARTICOLO_FIGLIO_COD_CONC': 'CODICE_FIGLIO'})
-    st.dataframe(df_merged[[
-        "CODICE_FIGLIO", "ARTICOLO_FIGLIO", "ARTICOLO_FIGLIO_DESCRIZIONE",
-        "CODICE", "CODICE_GREZZO", "COEFFICIENTE",
+if not df_merged_filtered.empty:
+    st.subheader("Distinta base componente")
+    df_merged_filtered = df_merged_filtered.rename(columns={'ARTICOLO_FIGLIO_COD_CONC': 'CODICE_FIGLIO'})
+    st.dataframe(df_merged_filtered[[
+        "CODICE_FIGLIO", "ARTICOLO_FIGLIO", "COEFFICIENTE","ARTICOLO_FIGLIO_DESCRIZIONE",
+        "CODICE", "CODICE_GREZZO",
         "L_BARRA", "GR/ML", "PESO_GREZZO_KG"
     ]], use_container_width=True)
 
     # Download CSV
-    with col2:
-        csv_df_merged = df_merged.to_csv(index=False)
-        st.download_button("Scarica distinta componente", data=csv_df_merged, file_name="distinta_filtrata.csv", mime="text/csv")
-        csv_df_merged_complete=df_merged_complete.to_csv(index=False)
-        st.download_button("Scarica distinta completa", data=csv_df_merged_complete, file_name="distinta_completa.csv", mime="text/csv")
+    csv_df_merged_filtered = df_merged_filtered.to_csv(index=False)
+    st.sidebar.download_button("Distinta componente", data=csv_df_merged_filtered, file_name="distinta_filtrata.csv", mime="text/csv")
+    csv_df_merged_complete=df_merged_complete.to_csv(index=False)
+    st.sidebar.download_button("Distinta completa", data=csv_df_merged_complete, file_name="distinta_completa.csv", mime="text/csv")
 
 else:
     st.error("Nessuna combinazione trovata per i filtri selezionati.")
